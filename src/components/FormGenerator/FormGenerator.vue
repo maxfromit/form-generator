8d1a8d2975f2b9d3b47cc3624716bf429944a869
maximfromit <bigmaxcapacity@gmail.com>
2025-03-27 11:56:26 +0300

    src/components/FormGenerator update form layout and styling for improved responsiveness

--- src/components/FormGenerator/FormGenerator.vue
+++ src/components/FormGenerator/FormGenerator.vue
@@ -17,49 +17,56 @@ const emit = defineEmits<{
 
 <template>
   model {{ model }}
-  <form @submit.prevent="emit('save')" v-if="model" class="flex flex-col gap-1">
+  <form
+    @submit.prevent="emit('save')"
+    v-if="model"
+    class="flex flex-col gap-1 flex-grow"
+    style="padding-bottom: 1rem"
+  >
     <!-- Render dynamic form fields -->
-    <div v-for="(field, index) in structure" :key="field.id">
-      <slot :name="`field_${field.id}`">
-        <!-- Default content if no slot is provided -->
-        <label :for="`field-${field.id}`">{{ field.label }}</label>
+    <div class="flex flex-col gap-1 flex-grow">
+      <div v-for="(field, index) in structure" :key="field.id">
+        <slot :name="`field_${field.id}`">
+          <!-- Default content if no slot is provided -->
+          <label :for="`field-${field.id}`">{{ field.label }}</label>
 
-        <!-- Input field -->
-        <input
-          v-if="field.type === 'input'"
-          v-model="model[index]"
-          :id="`field-${field.id}`"
-          :type="field.dataType === 'number' ? 'number' : 'text'"
-        />
+          <!-- Input field -->
+          <input
+            v-if="field.type === 'input'"
+            v-model="model[index]"
+            :id="`field-${field.id}`"
+            :type="field.dataType === 'number' ? 'number' : 'text'"
+          />
 
-        <!-- Textarea field -->
-        <textarea
-          v-if="typeof model[index] !== 'boolean' && field.type === 'textarea'"
-          :id="`field-${field.id}`"
-          v-model="model[index]"
-        ></textarea>
+          <!-- Textarea field -->
+          <textarea
+            v-if="typeof model[index] !== 'boolean' && field.type === 'textarea'"
+            :id="`field-${field.id}`"
+            v-model="model[index]"
+          ></textarea>
 
-        <!-- Checkbox field -->
-        <input
-          v-if="field.type === 'checkbox'"
-          v-model="model[index]"
-          type="checkbox"
-          :id="`field-${field.id}`"
-          :checked="model[index] === 'true' ? true : false"
-        />
+          <!-- Checkbox field -->
+          <input
+            v-if="field.type === 'checkbox'"
+            v-model="model[index]"
+            type="checkbox"
+            :id="`field-${field.id}`"
+            :checked="model[index] === 'true' ? true : false"
+          />
 
-        <!-- Select field -->
-        <select v-if="field.type === 'select'" :id="`field-${field.id}`" v-model="model[index]">
-          <option v-for="option in field.options" :key="option" :value="option">
-            {{ option }}
-          </option>
-        </select>
-      </slot>
+          <!-- Select field -->
+          <select v-if="field.type === 'select'" :id="`field-${field.id}`" v-model="model[index]">
+            <option v-for="option in field.options" :key="option" :value="option">
+              {{ option }}
+            </option>
+          </select>
+        </slot>
+      </div>
     </div>
-
     <!-- Buttons -->
-    <div class="flex gap-1">
-      <button type="button" @click="emit('discard')" :disabled="isDiscardDisabled">Discard</button>
+
+    <div class="flex gap-1 justify-end">
+      <button type="button" @click="emit('discard')">Discard</button>
       <button type="submit">Save</button>
     </div>
   </form>
