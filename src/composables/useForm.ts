07bd6e2e2afe4afdf3901e89f6023ac32f59c17f
maxfromit <bigmaxcapacity@gmail.com>
2026-01-18 20:41:30 +0300

    refactor(useForm): improve null checks and simplify resolvedFormValues logic

--- src/composables/useForm.ts
+++ src/composables/useForm.ts
@@ -10,30 +10,30 @@ const useForm = (dataKey: Ref<string>, sourceKey?: Ref<string>) => {
   const store = useStore()
 
   const formValuesFromHardCoded = computed(() =>
-    !!hardcodedFormData[dataKey.value] && hardcodedFormData[dataKey.value].values
+    !!hardcodedFormData[dataKey.value] && hardcodedFormData[dataKey.value]?.values
       ? hardcodedFormData[dataKey.value].values
       : [],
   )
 
   const formValuesFromLocal = computed(() =>
-    store.state.localFormValues && store.state.localFormValues[dataKey.value]?.values?.length > 0
-      ? store.state.localFormValues[dataKey.value]?.values
+    store.state.localFormValues && !!store.state.localFormValues[dataKey.value]?.values?.length
+      ? store.state.localFormValues[dataKey.value].values
       : [],
   )
 
   const formStructure = computed(() =>
-    dataKey && hardcodedFormData[dataKey.value].structure
+    dataKey && hardcodedFormData[dataKey.value]?.structure
       ? hardcodedFormData[dataKey.value].structure
       : [],
   )
 
   const resolvedFormValues = computed(() => {
-    if (sourceKey?.value === 'local' && formValuesFromLocal.value?.length > 0) {
-      return [...formValuesFromLocal.value]
+    if (sourceKey?.value) {
+      if (sourceKey?.value === 'local')
+        if (!!formValuesFromLocal.value?.length) return [...formValuesFromLocal.value]
     }
-    if ((!sourceKey || sourceKey.value !== 'local') && formValuesFromHardCoded.value?.length > 0) {
-      return [...formValuesFromHardCoded.value]
-    }
+
+    if (!!formValuesFromHardCoded.value?.length) return [...formValuesFromHardCoded.value]
 
     return getClearedValues(formStructure.value)
   })
